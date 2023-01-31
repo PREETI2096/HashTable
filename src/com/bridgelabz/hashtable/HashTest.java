094e76f3af2b4eda92b1489c5d84dd0e3e793fe9
PREETI2096 <preetirg96@gmail.com>
2023-01-31 11:57:09 +0530

    Added : hashtable program

---
+++ src/com/bridgelabz/hashtable/HashTest.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.hashtable;
+
+public class HashTest {
+	public static void main(String[] args) {
+		String str = "To be or not to be";
+		HashTable hash = new HashTable(6);
+		hash.countWords(str);
+
+	}
+}
